c7fb826205330882ee6a16a81df86c2d12213bcb
Sue Hütter <sh@love-it.at>
2021-07-29 11:39:11 +0200

    COMMIT x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
   ],
   imports: [
     BrowserModule,
